7ded3d23630f8cf4524d1af9e4c61184c95715b6
nacho13gamer <nachogomezcastillo447@gmail.com>
2024-12-11 20:13:42 -0600

    Rotacion del app
    uwu

--- Siembravalores/app/src/main/java/com/example/siembravalores/MainActivity.kt
+++ Siembravalores/app/src/main/java/com/example/siembravalores/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.siembravalores.ui.theme.SiembraValoresTheme
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        requestedOrientation = SCREEN_ORIENTATION_PORTRAIT
         setContent {
             SiembraValoresTheme {
                 SiembraValoresApp()
